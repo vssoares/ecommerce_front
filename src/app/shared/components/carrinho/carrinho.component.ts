0a1f72f55e37be3311bea11a277ebd092bbdca7a
Vinicius Soares <45067354+vssoares@users.noreply.github.com>
2023-06-19 17:45:53 -0300

    animacao remover produto carrinho

--- src/app/shared/components/carrinho/carrinho.component.ts
+++ src/app/shared/components/carrinho/carrinho.component.ts
@@ -83,7 +83,10 @@ export class CarrinhoComponent implements OnDestroy {
     this.subs.push(
       this.carrinhoService.removerProdutoCarrinho(produto).subscribe({
         next: dados => {
-          this.carrinhoService.setDadosCarrinho(dados);
+          // Remove o item do carrinho após um tempo para permitir a execução da animação
+          setTimeout(() => {
+            this.carrinhoService.setDadosCarrinho(dados);
+          }, 1000);
         },
       })
     );
